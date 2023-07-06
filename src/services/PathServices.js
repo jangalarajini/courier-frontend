30b2673a79f35f826a0dfe85d8a745973cfa0ae0
satyasaiteja123 <133064488+satyasaiteja123@users.noreply.github.com>
2023-07-05 21:29:35 -0500

    Create PathServices.js

---
+++ src/services/PathServices.js
@@ -0,0 +1,19 @@
+import apiClient from "./services";
+
+export default {
+  getPaths() {
+    return apiClient.get("path");
+  },
+  getPath(id) {
+    return apiClient.get("path/" + id);
+  },
+  addPath(path) {
+    return apiClient.post("path", path);
+  },
+  updatePath(path) {
+    return apiClient.put("path/" + path.id, path);
+  },
+  deletePath(pathId) {
+    return apiClient.delete("path/" + pathId);
+  },
+};
